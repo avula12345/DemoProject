dc31c55c4c36f9247f498517b6cce21a3debb287
avula12345 <ramamohan.avula@gmail.com>
2017-08-19 22:02:08 +0530

    Automated browser functionality
    Automated browser functionality

---
+++ src/test/java/org/liveproject/com/SeleniumTests/OpenBrowser.java
@@ -0,0 +1,20 @@
+package org.liveproject.com.SeleniumTests;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+	
+	@Test
+	public void openbrowser(){
+	
+	System.setProperty("webdriver.gecko.driver", "E:\\IMP\\geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().window().maximize();
+	driver.get("http://www.hdfcbank.com/");
+	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	driver.quit();
+	}
+}
